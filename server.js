c43faa46bf4796c24234130ea09a1617a2499b01
derialdavi <ficarra.davide290@gmail.com>
2023-02-08 14:31:23 +0100

    modified engine

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
+const hbs = require('hbs');
 const express = require('express');
 
 const PORT = 8080;
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {showTitle: true, foo: 'foo'});
 });
 
 app.get('/sendFile', (req, res) => {
-    res.send('test');
+    console.log(req.file);
+    res.redirect('/');
 });
 
 app.listen(PORT, () => {
