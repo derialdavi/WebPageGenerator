ccb8a191bf67deb9b3a573fd94bca818615db6c5
derialdavi <ficarra.davide290@gmail.com>
2023-02-08 12:34:52 +0100

    mamma mia

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const express = require('express');
 const PORT = 8080;
 
 const app = express();
-app.set('view engin', 'ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// app.get()
+app.get('/sendFile', (req, res) => {
+    res.send('test');
+});
 
 app.listen(PORT, () => {
     console.log('Server at %s', PORT);
