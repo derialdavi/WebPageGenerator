2df3c8a00d7b982fb4d80cd374fd109a9b6b073c
cisoh <cisonoh.44ggg@gmail.com>
2023-02-14 16:21:08 +0100

    cose inutili perche sono css

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,7 @@ input.addEventListener('change', () => {
         $('#img-selector').html('');
         for (var i = 0; i < sections.length; i++) {
             let text = $('#img-selector').html();
-            $('#img-selector').html(text + '<label for="img-selector'+ parseInt(i+1) +'" class="button-85"> carica immagine n.'+ parseInt(i+1) +' </label><input type="file" name="image' + parseInt(i+1) + '" id="img-selector' + parseInt(i+1) + '" accept="image/*">')
+            $('#img-selector').html(text + '<label for="img-selector'+ parseInt(i+1) +'" class="button-85"> carica immagine sezione n.'+ parseInt(i+1) +' </label><input type="file" name="image' + parseInt(i+1) + '" id="img-selector' + parseInt(i+1) + '" accept="image/*">')
         }
     };
     reader.readAsText(input.files[0]);
